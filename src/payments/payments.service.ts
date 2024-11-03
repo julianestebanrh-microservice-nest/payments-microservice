a0f532ca99321971e578d41a2fc286de304df682
Julian Esteban <julianestebanrh.contact@gmail.com>
2024-11-03 15:14:24 -0500

    ✨ New feature: Añadiendo conexion nats
    Description:

--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -1,12 +1,16 @@
-import { Injectable } from '@nestjs/common';
-import { envs } from 'src/config';
+import { Inject, Injectable, Logger } from '@nestjs/common';
+import { envs, NATS_SERVICE } from 'src/config';
 import Stripe from 'stripe';
 import { CreateSessionDto } from './dto';
 import { Request, Response } from 'express';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
 export class PaymentsService {
   private readonly stripe = new Stripe(envs.stripeSecret);
+  private readonly logger = new Logger('PaymentService');
+
+  constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
 
   async createSession(createSessionDto: CreateSessionDto) {
     const { currency, orderId, items } = createSessionDto;
@@ -60,9 +64,13 @@ export class PaymentsService {
     switch (event.type) {
       case 'charge.succeeded':
         const chargeSucceeded = event.data.object;
-        console.log({
-          metadata: chargeSucceeded.metadata,
-        });
+        const payload = {
+          paidId: chargeSucceeded.id,
+          orderId: chargeSucceeded.metadata.orderId,
+          receiptUrl: chargeSucceeded.receipt_url,
+        };
+
+        this.client.emit('order.payment', payload);
         break;
       default:
         console.log(`Event ${event.type} not handled.`);
